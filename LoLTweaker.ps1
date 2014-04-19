bc5a2fefd5beb21e2fd718718fa25560e0c9fd24
Loggan08 <ilja.korsun@gmail.com>
2014-04-19 04:47:44 +0200

    oops

--- LoLTweaker.ps1
+++ LoLTweaker.ps1
@@ -4,21 +4,21 @@ $Host.UI.RawUI.WindowTitle = "LoLTweaker $sScriptVersion"
 
 Function restore {
 cd "C:\Downloads\Backup"
-Move-Item "dbghelp.dll" "$LoL\solutions\lol_game_client_sln\releases\$sln\deploy"
-Move-Item "msvcp120.dll." "$LoL\solutions\lol_game_client_sln\releases\$sln\deploy"
-Move-Item "msvcr120.dll" "$LoL\solutions\lol_game_client_sln\releases\$sln\deploy"
-Move-Item "cg.dll" "$LoL\solutions\lol_game_client_sln\releases\$sln\deploy"
-Move-Item "cgD3D9.dll" "$LoL\solutions\lol_game_client_sln\releases\$sln\deploy"
-Move-Item "cgGL.dll" "$LoL\solutions\lol_game_client_sln\releases\$sln\deploy"
-Move-Item "dbghelp.dll" "$LoL\projects\lol_air_client\releases\$air\deploy"
-Move-Item "tbb.dll" "$LoL\solutions\lol_game_client_sln\releases\$sln\deploy"
-Move-Item "NPSWF32.dll" "$LoL\projects\lol_air_client\releases\$air\deploy\Adobe AIR\Versions\1.0\Resources"
-Move-Item "Adobe Air.dll" "$LoL\projects\lol_air_client\releases\$air\deploy\Adobe AIR\Versions\1.0"
-Move-Item "cg.dll" "$LoL\projects\lol_launcher\releases\$launch\deploy"
-Move-Item "cgD3D9.dll" "$LoL\projects\lol_launcher\releases\$launch\deploy"
-Move-Item "cgGL.dll" "$LoL\projects\lol_launcher\releases\$launch\deploy"
-Move-Item "msvcp120.dll" "$LoL\projects\lol_launcher\releases\$launch\deploy"
-Move-Item "msvcr120.dll" "$LoL\projects\lol_launcher\releases\$launch\deploy"
+Move-Item "dbghelp.dll" "RADS\solutions\lol_game_client_sln\releases\$sln\deploy"
+Move-Item "msvcp120.dll." "RADS\solutions\lol_game_client_sln\releases\$sln\deploy"
+Move-Item "msvcr120.dll" "RADS\solutions\lol_game_client_sln\releases\$sln\deploy"
+Move-Item "cg.dll" "RADS\solutions\lol_game_client_sln\releases\$sln\deploy"
+Move-Item "cgD3D9.dll" "RADS\solutions\lol_game_client_sln\releases\$sln\deploy"
+Move-Item "cgGL.dll" "RADS\solutions\lol_game_client_sln\releases\$sln\deploy"
+Move-Item "dbghelp.dll" "RADS\projects\lol_air_client\releases\$air\deploy"
+Move-Item "tbb.dll" "RADS\solutions\lol_game_client_sln\releases\$sln\deploy"
+Move-Item "NPSWF32.dll" "RADS\projects\lol_air_client\releases\$air\deploy\Adobe AIR\Versions\1.0\Resources"
+Move-Item "Adobe Air.dll" "RADS\projects\lol_air_client\releases\$air\deploy\Adobe AIR\Versions\1.0"
+Move-Item "cg.dll" "RADS\projects\lol_launcher\releases\$launch\deploy"
+Move-Item "cgD3D9.dll" "RADS\projects\lol_launcher\releases\$launch\deploy"
+Move-Item "cgGL.dll" "RADS\projects\lol_launcher\releases\$launch\deploy"
+Move-Item "msvcp120.dll" "RADS\projects\lol_launcher\releases\$launch\deploy"
+Move-Item "msvcr120.dll" "RADS\projects\lol_launcher\releases\$launch\deploy"
 exit
 }
 
@@ -29,27 +29,27 @@ cls
 Write-Host "Patching..."
 
 #Copying Items
-Copy-Item .\dbghelp.dll "$LoL\solutions\lol_game_client_sln\releases\$sln\deploy" | Out-String
-Copy-Item .\msvcp120.dll "$LoL\solutions\lol_game_client_sln\releases\$sln\deploy" | Out-String
-Copy-Item .\msvcr120.dll "$LoL\solutions\lol_game_client_sln\releases\$sln\deploy" | Out-String
-Copy-Item "$CG\cg.dll" "$LoL\solutions\lol_game_client_sln\releases\$sln\deploy" | Out-String
-Copy-Item "$CG\cgD3D9.dll" "$LoL\solutions\lol_game_client_sln\releases\$sln\deploy" | Out-String
-Copy-Item "$CG\cgGL.dll" "$LoL\solutions\lol_game_client_sln\releases\$sln\deploy" | Out-String
-Copy-Item .\dbghelp.dll "$LoL\projects\lol_air_client\releases\$air\deploy" | Out-String
-Copy-Item .\tbb.dll "$LoL\solutions\lol_game_client_sln\releases\$sln\deploy" | Out-String
-Copy-Item .\NPSWF32.dll "$LoL\projects\lol_air_client\releases\$air\deploy\Adobe AIR\Versions\1.0\Resources" | Out-String
-Copy-Item ".\Adobe Air.dll" "$LoL\projects\lol_air_client\releases\$air\deploy\Adobe AIR\Versions\1.0\" | Out-String
-Copy-Item "$CG\cg.dll" "$LoL\projects\lol_launcher\releases\$launch\deploy" | Out-String
-Copy-Item "$CG\cgD3D9.dll" "$LoL\projects\lol_launcher\releases\$launch\deploy" | Out-String
-Copy-Item "$CG\cgGL.dll" "$LoL\projects\lol_launcher\releases\$launch\deploy" | Out-String
-Copy-Item .\msvcp120.dll "$LoL\projects\lol_launcher\releases\$launch\deploy" | Out-String
-Copy-Item .\msvcr120.dll "$LoL\projects\lol_launcher\releases\$launch\deploy" | Out-String
+Copy-Item .\dbghelp.dll "RADS\solutions\lol_game_client_sln\releases\$sln\deploy" | Out-String
+Copy-Item .\msvcp120.dll "RADS\solutions\lol_game_client_sln\releases\$sln\deploy" | Out-String
+Copy-Item .\msvcr120.dll "RADS\solutions\lol_game_client_sln\releases\$sln\deploy" | Out-String
+Copy-Item "$CG\cg.dll" "RADS\solutions\lol_game_client_sln\releases\$sln\deploy" | Out-String
+Copy-Item "$CG\cgD3D9.dll" "RADS\solutions\lol_game_client_sln\releases\$sln\deploy" | Out-String
+Copy-Item "$CG\cgGL.dll" "RADS\solutions\lol_game_client_sln\releases\$sln\deploy" | Out-String
+Copy-Item .\dbghelp.dll "RADS\projects\lol_air_client\releases\$air\deploy" | Out-String
+Copy-Item .\tbb.dll "RADS\solutions\lol_game_client_sln\releases\$sln\deploy" | Out-String
+Copy-Item .\NPSWF32.dll "RADS\projects\lol_air_client\releases\$air\deploy\Adobe AIR\Versions\1.0\Resources" | Out-String
+Copy-Item ".\Adobe Air.dll" "RADS\projects\lol_air_client\releases\$air\deploy\Adobe AIR\Versions\1.0\" | Out-String
+Copy-Item "$CG\cg.dll" "RADS\projects\lol_launcher\releases\$launch\deploy" | Out-String
+Copy-Item "$CG\cgD3D9.dll" "RADS\projects\lol_launcher\releases\$launch\deploy" | Out-String
+Copy-Item "$CG\cgGL.dll" "RADS\projects\lol_launcher\releases\$launch\deploy" | Out-String
+Copy-Item .\msvcp120.dll "RADS\projects\lol_launcher\releases\$launch\deploy" | Out-String
+Copy-Item .\msvcr120.dll "RADS\projects\lol_launcher\releases\$launch\deploy" | Out-String
 
 cls
-Set-Location $LoL
+Set-Location RADS
 Set-Location ..
 Start-Process .\lol.launcher.exe
-$tbb = (Get-Command "$LoL\solutions\lol_game_client_sln\releases\$sln\deploy\tbb.dll").FileVersionInfo.FileVersion
+$tbb = (Get-Command "RADS\solutions\lol_game_client_sln\releases\$sln\deploy\tbb.dll").FileVersionInfo.FileVersion
       if($tbb -eq "4, 2, 0, 0"){
       Read-Host "Patch Successfull, Press any Key to do Windows Tweaks"
          exit }
@@ -67,11 +67,6 @@ Stop-Process -ProcessName LoLClient | out-null
 
 
 # These are not included in Powershell by default
-cls
-New-PSDrive HKCR Registry HKEY_CLASSES_ROOT
-cls
-New-PSDrive HKU Registry HKEY_CURRENT_USER
-cls
 
 #Finds script directory
 $dir = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
@@ -80,9 +75,8 @@ $dir = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
 #Todo: Add PMB Uninstall
 
 
-# Finds the LoL Directory from registry
 
-$LoL = Get-ItemProperty "HKCR:\VirtualStore\MACHINE\SOFTWARE\Wow6432Node\Riot Games\RADS" | Select-Object -ExpandProperty "LocalRootFolder"
+
 
 #Nvidia CG Directory
 
@@ -90,7 +84,7 @@ $CG = Get-ItemProperty "HKU:\Environment" | Select-Object -ExpandProperty "CG_BI
 
 # Setting variables for the latest LoL Updates
 
-Set-Location $LoL\solutions\lol_game_client_sln\releases
+Set-Location RADS\solutions\lol_game_client_sln\releases
 $sln = gci | ? {$_.PSIsContainer} | sort CreationTime -desc | select -f 1 | Select-Object -ExpandProperty "Name"
 
 Move-Item "dbghelp.dll" "C:\Downloads\Backup" | Out-string
@@ -102,7 +96,7 @@ Move-Item "cgGL.dll" "C:\Downloads\Backup" | Out-string
 Move-Item "tbb.dll" "C:\Downloads\Backup" | Out-string
 
 
-Set-Location "$LoL\projects\lol_launcher\releases"
+Set-Location "RADS\projects\lol_launcher\releases"
 $launch = gci | ? {$_.PSIsContainer} | sort CreationTime -desc | select -f 1 | Select-Object -ExpandProperty "Name"
 
 Move-Item "dbghelp.dll" "C:\Downloads\Backup" | Out-string
@@ -114,7 +108,7 @@ Move-Item "cgD3D9.dll" "C:\Downloads\Backup" | Out-string
 Move-Item "cgGL.dll" "C:\Downloads\Backup" | Out-string
 
 
-Set-Location $LoL\solutions\lol_air_client\releases
+Set-Location RADS\solutions\lol_air_client\releases
 $air = gci | ? {$_.PSIsContainer} | sort CreationTime -desc | select -f 1 | Select-Object -ExpandProperty "Name"
 Set-Location "$air\deploy\adobe air\versions\1.0\"
 Move-Item "Adobe Air.dll" "C:\Downloads\Backup" | Out-string
